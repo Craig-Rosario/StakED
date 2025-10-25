47d80ae79ae0cbd65b29abfc350577f39d0c9be0
Craig Rosario <craig.rosario04@gmail.com>
2025-10-25 14:30:09 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,17 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",               
+      "http://localhost:3000",              
+      "https://stak-ed-frontend.vercel.app", 
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
